0c0e70bb4b642ba35c3642344cff899a187a4197
Seth Thomas <romanronin92@gmail.com>
2018-04-23 00:50:09 -0400

    testing textarea results for die rolls

--- components/plugin-cards/dice-roller/dice-roller.component.js
+++ components/plugin-cards/dice-roller/dice-roller.component.js
@@ -10,6 +10,8 @@
     self.clearButton = true;
     initAmnt();
     initResults();
+    initRadioModifier();
+    initModifier();
 
     self.roll = (dienum) => {
       clearResult(dienum);
@@ -32,6 +34,8 @@
     }
 
     self.clear = () => {
+      initModifier();
+      initRadioModifier();
       initAmnt();
       initResults();
       self.total = null;
@@ -43,6 +47,8 @@
       || (self.amnt.d4 < 1 && self.amnt.d8 < 1 && self.amnt.d10 < 1 && self.amnt.d100 < 1 && self.amnt.d12 < 1 && self.amnt.d20 < 1);
     }
 
+    function initModifier() { self.modifier = { "d4": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
+    function initRadioModifier() { self.radioModifier = { "d4": 'plus', "d8": 'plus', "d10": 'plus', "d100": 'plus', "d12": 'plus', "d20": 'plus' } }
     function initAmnt() { self.amnt = { "d4": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
     function setTotal() { self.total = self.results.d4 + self.results.d8 + self.results.d10 + self.results.d100 + self.results.d12 + self.results.d20; }
     function initResults() { self.results = { "d4": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
