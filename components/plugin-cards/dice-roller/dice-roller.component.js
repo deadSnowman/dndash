990f4c4a00d0c879bc733e4d270e25dfdf116ef9
Seth Thomas <romanronin92@gmail.com>
2018-04-24 00:16:52 -0400

    input groups for die rolling

--- components/plugin-cards/dice-roller/dice-roller.component.js
+++ components/plugin-cards/dice-roller/dice-roller.component.js
@@ -24,6 +24,7 @@
 
     self.submit = () => {
       self.roll(4);
+      self.roll(6);
       self.roll(8);
       self.roll(10);
       self.roll(100);
@@ -43,15 +44,15 @@
     }
 
     self.totalDisabled = () => {
-      return !(self.amnt.d4 != null || self.amnt.d8 != null || self.amnt.d10 != null || self.amnt.d100 != null || self.amnt.d12 != null || self.amnt.d20 != null)
-      || (self.amnt.d4 < 1 && self.amnt.d8 < 1 && self.amnt.d10 < 1 && self.amnt.d100 < 1 && self.amnt.d12 < 1 && self.amnt.d20 < 1);
+      return !(self.amnt.d4 != null || self.amnt.d6 != null || self.amnt.d8 != null || self.amnt.d10 != null || self.amnt.d100 != null || self.amnt.d12 != null || self.amnt.d20 != null)
+      || (self.amnt.d4 < 1 && self.amnt.d6 < 1 && self.amnt.d8 < 1 && self.amnt.d10 < 1 && self.amnt.d100 < 1 && self.amnt.d12 < 1 && self.amnt.d20 < 1);
     }
 
-    function initModifier() { self.modifier = { "d4": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
-    function initRadioModifier() { self.radioModifier = { "d4": 'plus', "d8": 'plus', "d10": 'plus', "d100": 'plus', "d12": 'plus', "d20": 'plus' } }
-    function initAmnt() { self.amnt = { "d4": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
-    function setTotal() { self.total = self.results.d4 + self.results.d8 + self.results.d10 + self.results.d100 + self.results.d12 + self.results.d20; }
-    function initResults() { self.results = { "d4": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
+    function initModifier() { self.modifier = { "d4": null, "d6": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
+    function initRadioModifier() { self.radioModifier = { "d4": 'plus', "d6": 'plus', "d8": 'plus', "d10": 'plus', "d100": 'plus', "d12": 'plus', "d20": 'plus' } }
+    function initAmnt() { self.amnt = { "d4": null, "d6": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
+    function setTotal() { self.total = self.results.d4 + self.results.d6 + self.results.d8 + self.results.d10 + self.results.d100 + self.results.d12 + self.results.d20; }
+    function initResults() { self.results = { "d4": null, "d6": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
     function clearResult(dienum) { self.results["d" + dienum] = null; }
   }
 
