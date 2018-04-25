5933cc91df66eb34dea0c39e6407e63ab8f703a5
Seth Thomas <romanronin92@gmail.com>
2018-04-25 00:13:44 -0400

    Clicking die buttons rolls 1 if not specified

--- components/plugin-cards/dice-roller/dice-roller.component.js
+++ components/plugin-cards/dice-roller/dice-roller.component.js
@@ -5,6 +5,13 @@
     module('diceRoller').
     controller('DiceRollerCtrl', DiceRollerCtrl);
 
+  /**
+   * Dice roller made to sort of resembled Wizard's official roller
+   * Clicking the individual die buttons rolls the associated die
+   * Rolling all die, rolls all die with a number > 0
+   * Add a +/- modifier to each type of die roll like you would with your stat rolls or whatever in DnD
+   * Clear button wipes everything
+   */
   function DiceRollerCtrl(diceRollerService) {
     var self = this;
     self.clearButton = true;
@@ -13,8 +20,10 @@
     initRadioModifier();
     initModifier();
 
+    // roll die from d# button click
     self.roll = (dienum) => {
       clearResult(dienum);
+      if(self.amnt["d" + dienum] == null || self.amnt["d" + dienum] == 0) self.amnt["d" + dienum] = 1; // if no amnt, roll 1 die
       for (let i = 0; i < self.amnt["d" + dienum]; i++) {
         self.results["d" + dienum] = self.results["d" + dienum] + Math.floor(Math.random() * dienum) + 1;
       }
@@ -22,18 +31,27 @@
       self.clearButton = false;
     }
 
-    self.submit = () => {
-      self.roll(4);
-      self.roll(6);
-      self.roll(8);
-      self.roll(10);
-      self.roll(100);
-      self.roll(12);
-      self.roll(20);
+    // button click for rolling all die that have a specified amount.  Amnt 0 or null wipes result for related die on rollAll
+    self.rollAll = () => {
+      initResults();
+      let set = [4, 6, 8, 10, 100, 12, 20];
+      set.forEach(dienum => {
+        for (let i = 0; i < self.amnt["d" + dienum]; i++) {
+          self.results["d" + dienum] = self.results["d" + dienum] + Math.floor(Math.random() * dienum) + 1;
+        }
+      });
       setTotal();
       self.clearButton = false;
     }
 
+    // roll all die, set total, and show the clear button
+    self.submit = () => {
+      self.rollAll();
+      setTotal();
+      self.clearButton = false;
+    }
+
+    // clear everything
     self.clear = () => {
       initModifier();
       initRadioModifier();
@@ -43,11 +61,13 @@
       self.clearButton = true;
     }
 
+    // Conditions for hiding the roll all button.  Basically checks if there's > 1 somewhere in the amnt input fields
     self.totalDisabled = () => {
       return !(self.amnt.d4 != null || self.amnt.d6 != null || self.amnt.d8 != null || self.amnt.d10 != null || self.amnt.d100 != null || self.amnt.d12 != null || self.amnt.d20 != null)
       || (self.amnt.d4 < 1 && self.amnt.d6 < 1 && self.amnt.d8 < 1 && self.amnt.d10 < 1 && self.amnt.d100 < 1 && self.amnt.d12 < 1 && self.amnt.d20 < 1);
     }
 
+    // Initializing data bound json compoments tied to dice-roller.html.  Results object is also in here.
     function initModifier() { self.modifier = { "d4": null, "d6": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
     function initRadioModifier() { self.radioModifier = { "d4": 'plus', "d6": 'plus', "d8": 'plus', "d10": 'plus', "d100": 'plus', "d12": 'plus', "d20": 'plus' } }
     function initAmnt() { self.amnt = { "d4": null, "d6": null, "d8": null, "d10": null, "d100": null, "d12": null, "d20": null } }
