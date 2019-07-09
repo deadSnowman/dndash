714c0df5fbe729de4ee1c175679c999800bc6f2d
Seth Thomas <romanronin92@gmail.com>
2019-07-09 02:35:22 -0400

    Constants and some logic for stat roller

--- components/plugin-cards/stat-roller/stat-roller.component.js
+++ components/plugin-cards/stat-roller/stat-roller.component.js
@@ -3,33 +3,32 @@
 
   angular.
     module('statRoller').
-    controller('StatRollerCtrl', StatRollerCtrl);
+    controller('StatRollerCtrl', ['races', StatRollerCtrl]);
 
   /**
-   * Add docs
+   * Roll starting stats for different DnD races
    */
-  function StatRollerCtrl() {
-    this.test= "test";
+  function StatRollerCtrl(races) {
+    this.selectedRace = "-1";
+    this.selectedSubrace = "-1";
+    this.races = races;
+    this.abilityScore = { str: 0, dex: 0, con: 0, int: 0, wis: 0, chr: 0 };
 
-    this.selectedRace = null;
-
-    this.races = [
-      {
-        id: 1,
-        name: "Aarakocra" 
-      },
-      {
-        id: 2,
-        name: "Aasimar",
-        subraces: [
-          "Protector",
-          "Scourge",
-          "Fallen"
-        ]
+    this.hasSubrace = () => {
+      let race = this.races[this.selectedRace];
+      if (race && race.subraces) {
+        return race.subraces.length > 0;
+      } else {
+        return false;
       }
-    ];
+    }
 
+    this.getSubrace = () => {
+      return this.races[this.selectedRace].subraces;
+    }
 
+    this.resetSubrace = () => {
+      this.selectedSubrace = "-1";
+    }
   }
-
 })();
--- components/plugin-cards/stat-roller/stat-roller.module.js
+++ components/plugin-cards/stat-roller/stat-roller.module.js
@@ -1,5 +1,16 @@
 (function() {
   'use strict';
   
-  angular.module('statRoller', []);
+  angular.module('statRoller', [])
+  .constant('races', [
+    { name: "Aarakocra", abilityScoreIncrease: { dex: 2, wis: 1 } },
+    {
+      name: "Aasimar",
+      subraces: [
+        { name: "Protector", abilityScoreIncrease: { wis: 1, chr: 2 } },
+        { name: "Scourge", abilityScoreIncrease: { con: 1, chr: 2 } },
+        { name: "Fallen", abilityScoreIncrease: { str: 1, chr: 2 } }
+      ]
+    }
+  ]);
 })();
